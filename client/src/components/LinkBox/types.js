8a4e07ecba5f0e772c1634aff567e2e9b0227772
frank shin <im@frnkshin.io>
2019-10-20 22:44:32 -0400

    removed client/

--- client/src/components/LinkBox/types.js
+++
@@ -1,3 +0,0 @@
-export const ON_CHANGE = 'ON_CHANGE';
-export const ON_INIT = 'ON_INIT';
-export const ON_ERROR = 'ON_ERROR';
--- client/src/components/Timer/types.js
+++
@@ -1,2 +0,0 @@
-export const ON_INIT="ON_INIT";
-export const ON_TICK="ON_TICK";
